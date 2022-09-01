2903b1ddf531d5c03616aa6c7cf97d1466c18cc7
Gustavo Vieira <gustavovieirazu@gmail.com>
2022-09-01 11:23:22 -0300

    .

--- src/Game/index.tsx
+++ src/Game/index.tsx
@@ -1,10 +1,11 @@
 import Layout from "../components/layout";
+import * as C from "./style";
 
 function Game() {
     return (
-        <>
+        <C.Container>
             <Layout />
-        </>
+        </C.Container>
     );
 }
 export default Game;
